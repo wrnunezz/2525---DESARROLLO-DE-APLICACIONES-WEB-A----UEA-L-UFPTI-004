30f578e611f28dc15c561f6fd621b1adf7fb1d41
wrnunezz <wr.nunezz@uea.edu.ec>
2025-08-13 18:36:32 -0500

    Actulización Semana 09

---
+++ Proyecto/index.py
@@ -0,0 +1,15 @@
+# ---------------------------
+# Semana 9 - Proyecto Flask
+# ---------------------------
+
+# 📌 Requisitos para iniciar el proyecto:
+
+# 1. Crear carpeta del proyecto:       mkdir Proyecto
+# 2. Ingresar a la carpeta:            cd Proyecto
+# 3. Instalar virtualenv:              pip install virtualenv
+# 4. Crear entorno virtual:            python -m venv venv   # Virtual Environment
+# 5. Activar entorno virtual:          venv\Scripts\activate   # Windows
+#                                      source venv/bin/activate   # Mac/Linux
+# 6. Instalar Flask:                   pip install flask
+# 7. Ejecutar la aplicación Flask:     python app.py
+# 8. Abrir navegador y acceder a:     http://localhost:5000/
