e163ed63c74df5fc09967ab8e48c550e597cad74
wrnunezz <wr.nunezz@uea.edu.ec>
2025-08-19 18:49:40 -0500

    Semana 10

--- Proyecto/app.py
+++ Proyecto/app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '¡Hola! Bienvenido a mi aplicación Flask.'
+    #return '¡Hola! Bienvenido a mi aplicación Flask.'
+    return render_template('index.html', title='Inicio')
+
 
 @app.route('/usuario/<nombre>')
 def usuario(nombre):
     return f'Bienvenido, {nombre}!'
+
+@app.route('/about/')
+def about():
+    return render_template('about.html', title='Acerca de')
 
 if __name__ == '__main__':
     app.run(debug=True)
